72994ad0c4c70b34273b1873efff138f24d6cc49
Bhavana4790 <99709516+Bhavana4790@users.noreply.github.com>
2024-01-03 16:49:41 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,56 @@
+import streamlit as st
+from PIL import Image
+import io
+import os
+from detection import *
+import base64
+
+st.title("Object Detection Streamlit App")
+
+# File uploader for video input
+video_file = st.file_uploader("Upload a video file", type=["mp4", "avi", "mov"])
+
+if video_file is not None:
+
+    # save the uploaded video
+    with open(video_file.name, "wb") as videos:
+        videos.write(video_file.read())
+
+    # Perform object detection on the uploaded video
+    st.info("Detecting objects. Please wait...")
+    video_bytes = video_file.read()
+    np_video = np.frombuffer(video_bytes, dtype=np.uint8)
+    video_cap = cv2.VideoCapture()
+    video_cap.open(video_file.name)
+    detection_data = main(video_file.name, "Output.avi")
+
+    # Display the processed video
+    # st.video(processed_video, format="video/mp4")
+
+    # Create a DataFrame with detection information
+    # detection_df = pd.DataFrame(detection_data, columns=["Class", "Confidence", "Box"])
+    st.write("Object Detection Results:")
+    st.write(detection_data)
+
+
+    # Save the DataFrame to Excel
+    filename = "Output.csv"
+    detection_data.to_csv(filename, index=False)
+    # st.info(f"Detection results saved to {excel_file.name}")
+
+    # Hyperlink to download the Excel file
+    # st.markdown(f"**Download Excel File:** [Download Excel](data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{filename})")
+    st.markdown(f"Download [CSV results](data:file/csv;base64,{base64.b64encode(open(filename, 'rb').read()).decode()})")
+
+    # Hyperlink to download the processed video
+    # video_file_name = video_file.name.split(".")[0] + "_processed.mp4"
+    # cv2.imwrite(video_file_name, processed_video)
+    video_file_name = "Output.avi"
+    result_video_bytes = open(video_file_name, "rb").read()
+    # st.markdown(f"**Download Processed Video:** [Download Video](data:video/mp4;base64,{video_file_name})")
+    # st.markdown(f"**Download Processed Video:** [Download Video](data:video/mp4;base64,{(result_video_bytes).decode()})")
+    st.markdown(f"**Download Video:** [Output Video](data:video/avi;base64,{base64.b64encode(result_video_bytes).decode()})")
+
+
+    if os.path.exists(video_file.name):
+        os.remove(video_file.name)
---
+++ detection.py
@@ -0,0 +1,162 @@
+from IPython import display
+display.clear_output()
+
+import ultralytics
+ultralytics.checks()
+
+import supervision as sv
+import numpy as np 
+import pandas as pd 
+import os
+import cv2
+from ultralytics import YOLO
+
+model_person = YOLO("yolov8x.pt")
+model_person.fuse()
+
+model_helmet = YOLO('best.pt')
+
+
+def excel_results(frame_num, result, names):
+  track_id = result.tracker_id
+  classes = [int(i.item()) for i in result.class_id]
+  bboxes = [[round(val.item()) for val in bx] for bx in result.xyxy]
+  cls_nm = [names[i] for i in classes]
+  Output = {
+      "Frame_num" : frame_num,
+      "Track_id": track_id,
+      "Classes" : classes,
+      "Coordinates" : bboxes,
+      "Prediction" : cls_nm,
+  }
+  return Output
+
+def callback1(frame: np.ndarray, 
+              index:int, 
+              selected_classes, 
+              byte_tracker, 
+              byte_tracker1,
+              trace_annotator,
+              box_annotator,
+              line_zone,
+              line_zone_annotator,
+              sink,
+              ):
+  results = model_person(frame, verbose=False)[0]
+  # results = results[np.isin(results.boxes.cls.int().cpu(), selected_classes)]
+  names = results.names
+
+  results1 = model_helmet(frame)[0]
+  names1 = results1.names
+
+  detections = sv.Detections.from_ultralytics(results)
+  detections = detections[np.isin(detections.class_id, selected_classes)]
+  # print("before tracking", detections)
+  detections = byte_tracker.update_with_detections(detections)
+  # print("after tracking: ",detections)
+
+  detections1 = sv.Detections.from_ultralytics(results1)
+  # print("before tracking", detections1)
+  detections1 = byte_tracker1.update_with_detections(detections1)
+  # print("after tracking: ",detections1)
+
+  Output1 = excel_results(index, detections, names)
+  Output2 = excel_results(index, detections1, names1)
+
+  # tracking code - video
+  labels = [
+      f"#{tracker_id} {model_person.model.names[class_id]} {confidence:0.2f}"
+      for _, _, confidence, class_id, tracker_id
+      in detections
+  ]
+
+  labels1 = [
+      f"#{tracker_id} {model_helmet.model.names[class_id]} {confidence:0.2f}"
+      for _, _, confidence, class_id, tracker_id
+      in detections1
+  ]
+
+  annotated_frame = trace_annotator.annotate(
+      scene=frame.copy(),
+      detections=detections
+  )
+  annotated_frame1 = trace_annotator.annotate(
+      scene=frame.copy(),
+      detections=detections1
+  )
+
+  annotated_frame=box_annotator.annotate(
+      scene=annotated_frame,
+      detections=detections,
+      labels=labels)
+
+  annotated_frame1 = box_annotator.annotate(
+      scene=annotated_frame1,
+      detections=detections1,
+      labels=labels1)
+
+  # update line counter
+  line_zone.trigger(detections1)
+
+  # return frame with box and line annotated result
+  line_zone_annotator.annotate(annotated_frame, line_counter=line_zone)
+  line_zone_annotator.annotate(annotated_frame1, line_counter=line_zone)
+
+  sink.write_frame(line_zone_annotator.annotate(annotated_frame, line_counter=line_zone))
+  sink.write_frame(line_zone_annotator.annotate(annotated_frame1, line_counter=line_zone))
+
+  return Output1, Output2
+
+def main(SOURCE_VIDEO_PATH, TARGET_VIDEO_PATH):
+    # create VideoInfo instance
+    video_info = sv.VideoInfo.from_video_path(SOURCE_VIDEO_PATH)
+
+    # create frame generator
+    # generator = sv.get_video_frames_generator(SOURCE_VIDEO_PATH)
+
+    # dict maping class_id to class_name
+    CLASS_NAMES_DICT = model_person.model.names
+
+    selected_classes = [0,1] # 0-person , 1- bicycle
+
+    LINE_START = sv.Point(10, 980)
+    LINE_END = sv.Point(500, 120)
+
+    byte_tracker = sv.ByteTrack(track_thresh=0.25, frame_rate=30)
+    byte_tracker1 = sv.ByteTrack(track_thresh=0.25, frame_rate=30)
+
+    # create LineZone instance, it is previously called LineCounter class
+    line_zone = sv.LineZone(start=LINE_START, end=LINE_END)
+
+    # create instance of BoxAnnotator
+    box_annotator = sv.BoxAnnotator(thickness=2, text_thickness=1, text_scale=1)
+
+    # create instance of TraceAnnotator
+    trace_annotator = sv.TraceAnnotator(thickness=1, trace_length=50)
+
+    # create LineZoneAnnotator instance, it is previously called LineCounterAnnotator class
+    line_zone_annotator = sv.LineZoneAnnotator(thickness=4, text_thickness=2)
+
+    Md1_res = []
+    Md2_res = []
+    with sv.VideoSink(target_path=TARGET_VIDEO_PATH, video_info=video_info) as sink:
+        for index, frame in enumerate(
+            sv.get_video_frames_generator(source_path=SOURCE_VIDEO_PATH)
+        ):
+            Res1, Res2 = callback1(frame, index, selected_classes, byte_tracker, byte_tracker1, trace_annotator, box_annotator, line_zone, line_zone_annotator, sink)
+            Md1_res.append(Res1)
+            Md2_res.append(Res2)
+            # break
+
+    Opt1 = pd.DataFrame(Md1_res, columns = Md1_res[0].keys())
+    Opt2 = pd.DataFrame(Md2_res, columns = Md2_res[0].keys())
+
+    merged_df = pd.merge(Opt1, Opt2, on='Frame_num', how='inner')
+    del merged_df['Classes_x']
+    del merged_df['Classes_y']
+
+    # merged_df.to_csv("Merged_Output.csv")
+    return merged_df
+
+    
+# print(main("kidicalmass (1).mp4", "Output.avi"))
